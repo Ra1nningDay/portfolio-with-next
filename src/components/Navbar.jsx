478118d52e76f12d794631f2076e9c223875a3c2
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-03-02 17:34:03 +0700

    build: route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,37 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return (
-        <nav className="flex justify-between h-[5rem] items-center px-4">
-            <div>
-                <h1 className="">Hello</h1>
-            </div>
-            <div>
-                <ul className="flex gap-5">
-                    <li>หน้าหลัก</li>
-                    <li>เกี่ยวกับ</li>
-                    <li>ติดต่อ</li>
-                </ul>
+        <nav className="">
+            <div className="container mx-auto">
+                <div className="flex justify-between py-4 items-center px-4 ">
+                    <div>
+                        <ul className="flex items-end gap-5">
+                            <Link href="/">
+                                <h1 className="text-2xl font-bold">
+                                    RainningDay
+                                </h1>
+                            </Link>
+                            <Link href="/bio">
+                                <span className="font-semibold text-lg">
+                                    Bio
+                                </span>
+                            </Link>
+                            <Link href="/projects">
+                                <span className="font-semibold text-lg">
+                                    Projects
+                                </span>
+                            </Link>
+                        </ul>
+                    </div>
+                    <div>
+                        <Link href="/contact">
+                            <span className="border-1 p-3 rounded-3xl hover:bg-gray-300">
+                                Contact
+                            </span>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </nav>
     );
